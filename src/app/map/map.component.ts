88b0cc349b2c7ec34fa9e78a78ae5a9520e094d9
csaflip <scolinadams@gmail.com>
2024-08-18 20:47:45 -0600

    mapp

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet-gpx';
+import GpxParser from 'gpxparser';
+import FitParser from 'fit-file-parser';
 
 @Component({
   selector: 'app-map',
@@ -15,7 +17,11 @@ export class MapComponent implements OnInit, AfterViewInit {
     L.marker([32.5568, 35.8469]) // Irbid
   ];
   gpx: string = "";
+  fit: string = "";
+  fitJson: Object = {};
   file:any;
+  maxElevation: number = 0;
+  totalDistance: number = 0; 
 
   constructor() { }
 
@@ -29,7 +35,53 @@ export class MapComponent implements OnInit, AfterViewInit {
   // );
   }
 
-  fileChanged(e: any) {
+  round(num: number, fractionDigits: number): number {
+    return Number(num.toFixed(fractionDigits));
+}
+
+  metersToFeet(meters: number): number {
+    return this.round(meters * 3.281, 2);
+  }
+
+  metersToMiles(meters: number): number {
+    return this.round(0.000621371*meters, 2);
+  }
+
+  parseFIT(): any {
+    let fitParser = new FitParser({
+      force: true,
+      speedUnit: 'km/h',
+      lengthUnit: 'km',
+      temperatureUnit: 'kelvin',
+      elapsedRecordField: true,
+      mode: 'cascade',
+    });
+
+      // Parse your file
+  fitParser.parse(this.fit,  (error: any, data: any) => {
+  
+    // Handle result of parse method
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.stringify(data));
+      this.fitJson = data;
+    }
+    
+  });
+    
+  }
+
+parseGPX(): any {
+  let parser = new GpxParser();
+  parser.parse(this.gpx);
+  this.totalDistance = this.metersToMiles(parser.tracks[0].distance.total);
+  this.maxElevation = this.metersToFeet(parser.tracks[0].elevation.max);
+  // console.log(totalDistance)
+
+}
+
+fileChanged(e: any) {
     this.file = e.target.files[0];
     this.uploadDocument(this.file)
 }
@@ -38,6 +90,8 @@ uploadDocument(file: any) {
   let fileReader = new FileReader();
   fileReader.onload = (e) => {
     this.gpx = fileReader.result as string;
+    //TODO
+    this.fit = fileReader.result as string;
   }
   fileReader.readAsText(this.file);
 }
